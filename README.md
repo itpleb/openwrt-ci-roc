5e10ee8504e84bcdbd474e5bc35f8e182d8ac555
WKLESS <wkless@qq.com>
2026-05-08 12:37:21 +0800

    ci: 更新默认IP地址和仓库URL
    将默认管理地址从192.168.2.1更改为192.168.7.1
    更新所有工作流文件和脚本中的仓库URL，从laipeng668更改为itpleb

--- README.md
+++ README.md
@@ -8,11 +8,11 @@
 - **本固件禁止用于任何商业用途，请务必严格遵守国家互联网使用相关法律规定！**
 
 ## 项目说明
-- 默认管理地址：**`192.168.2.1`** 默认用户：**`root`** 默认密码：**`none`**
+- 默认管理地址：**`192.168.7.1`** 默认用户：**`root`** 默认密码：**`none`**
 - 云编译来源：[haiibo](https://github.com/haiibo/OpenWrt) [视频教程](https://www.youtube.com/watch?v=6j4ofS0GT38&t=507s)
 
 ## 仓库说明
-- 本人 Fork 的仓库：[ImmortalWrt](https://github.com/laipeng668/immortalwrt) [LibWrt](https://github.com/laipeng668/openwrt-6.x)，内容大体一致。
+- 本人 Fork 的仓库：[ImmortalWrt](https://github.com/itpleb/immortalwrt) [LibWrt](https://github.com/itpleb/openwrt-6.x)，内容大体一致。
 - `ImmortalWrt` 和 `LibWrt` 分别通过 rebase 和 merge 进行更新，相互印证。
 - `LibWrt` 因为 DTS 更为丰富，所以支持更多的机型。
 
@@ -23,7 +23,7 @@
 - 插件对应名称及功能请参考恩山网友帖子：[OpenWrt软件包全量解释](https://www.right.com.cn/FORUM/forum.php?mod=viewthread&tid=8384897)。
 - 如需修改默认 IP、添加或删除插件包以及一些其他设置请在 `Roc-script.sh` 文件内修改。
 - 添加或修改 `xx.yml` 文件，最后点击 `Actions` 运行要编译的 `workflow` 即可开始编译。
-- 编译大概需要 1-2 小时，编译完成后在仓库主页 [Releases](https://github.com/laipeng668/openwrt-ci-roc/releases) 对应 Tag 标签内下载固件。
+- 编译大概需要 1-2 小时，编译完成后在仓库主页 [Releases](https://github.com/itpleb/openwrt-ci-roc/releases) 对应 Tag 标签内下载固件。
 
 ![Overview](Overview.png)
 ![Global](Global.png)
--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,5 +1,5 @@
 # 修改默认IP & 固件名称 & 编译署名和时间
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
 sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.release\.description + ' / ' : '') + (luciversion || ''),# \
             _('Firmware Version'),\n \
@@ -8,7 +8,7 @@ sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.r
                 ? boardinfo.release.description + ' / '\n \
                 : '') + (luciversion || '') + ' / ',\n \
             E('a', {\n \
-                href: 'https://github.com/laipeng668/openwrt-ci-roc/releases',\n \
+                href: 'https://github.com/itpleb/openwrt-ci-roc/releases',\n \
                 target: '_blank',\n \
                 rel: 'noopener noreferrer'\n \
                 }, [ 'Built by Roc $(date "+%Y-%m-%d %H:%M:%S")' ])\n \
@@ -46,12 +46,12 @@ function git_sparse_clone() {
 }
 
 # ariang & Go & frp & WolPlus & Argon & Aurora & OpenList & Lucky & wechatpush & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
-git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
-git_sparse_clone master https://github.com/laipeng668/packages lang/golang
+git_sparse_clone ariang https://github.com/itpleb/packages net/ariang
+git_sparse_clone master https://github.com/itpleb/packages lang/golang
 mv -f package/golang feeds/packages/lang/golang
-git_sparse_clone frp https://github.com/laipeng668/packages net/frp
+git_sparse_clone frp https://github.com/itpleb/packages net/frp
 mv -f package/frp feeds/packages/net/frp
-git_sparse_clone frp https://github.com/laipeng668/luci applications/luci-app-frpc applications/luci-app-frps
+git_sparse_clone frp https://github.com/itpleb/luci applications/luci-app-frpc applications/luci-app-frps
 mv -f package/luci-app-frpc feeds/luci/applications/luci-app-frpc
 mv -f package/luci-app-frps feeds/luci/applications/luci-app-frps
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
